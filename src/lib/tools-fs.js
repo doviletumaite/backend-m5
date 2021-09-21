654188ab00417004527a7999392fd89de10a3172
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-09-21 13:52:38 +0200

    crud backend

--- src/lib/tools-fs.js
+++ src/lib/tools-fs.js
@@ -2,15 +2,19 @@ import fs from "fs-extra"
 import { join, dirname } from "path"
 import { fileURLToPath } from "url"
 
-const { readJSON, writeJSON } = fs
+const { readJSON, writeJSON, writeFile } = fs
 
 const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data")
 
 const postsJSONPath = join(dataFolderPath, "posts.json")
 const authorsJSONPath = join(dataFolderPath, "authors.json")
 
+const publicFolderPath = join(process.cwd(), "./public/img")
+export const savePicture = (name, contentAsBuffer) => writeFile(join(publicFolderPath, name), contentAsBuffer)
+
 export const getPost = () => readJSON(postsJSONPath)
 export const getAuthors = () => readJSON(authorsJSONPath)
 
 export const writePosts = content => writeJSON(postsJSONPath, content)
 export const writeAuthors = content => writeJSON(authorsJSONPath, content)
+
--- src/services/files/index.js
+++ src/services/files/index.js
@@ -1,7 +1,18 @@
 import express from "express"
+import multer from "multer"
+import { savePicture } from "../../lib/tools-fs.js"
 
 const filesRouter = express.Router()
-filesRouter.post("/uploadAvatar/:id", (req, res, next)=>{
-    
+
+filesRouter.post("/uploadAvatar/:id",
+multer({})
+.single("picture"),
+async (req, res, next) => {
+    try {
+      await savePicture(req.file.originalname, req.file.buffer)  
+      res.send("OK")
+    } catch (error) {
+        next(error) 
+    }
 })
 export default filesRouter
