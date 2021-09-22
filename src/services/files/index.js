88e7144838006a120cfd67def7a55337e7b90c0b
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-09-22 17:56:03 +0200

    up

--- src/services/files/index.js
+++ src/services/files/index.js
@@ -3,15 +3,16 @@ import multer from "multer"
 import { getPDFReadableStream } from "../../lib/pdf.js"
 import { savePicture } from "../../lib/tools-fs.js"
 import { pipeline } from "stream"
-
+// http://localhost:3002/files/uploadPicture/:id
 const filesRouter = express.Router()
 
-filesRouter.post("/uploadPicture/:id",
+filesRouter.post("/uploadPicture",
 multer({})
 .single("picture"),
 async (req, res, next) => {
     try {
       await savePicture(req.file.originalname, req.file.buffer)  
+      console.log("picture:",req.file.originalname )
       res.send("OK")
     } catch (error) {
         next(error) 
